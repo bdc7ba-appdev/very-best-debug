a77c8c6a31766cabb2d5d6c664ed3c307becf7de
bdc7ba <bcrozier@chicagobooth.edu>
2022-11-15 20:25:18 +0000

    figured out venue update

--- config/routes.rb
+++ config/routes.rb
@@ -9,9 +9,10 @@ Rails.application.routes.draw do
 
   get("/venues", { :controller => "venues", :action => "index" })
   get("/insert_venue_record", { :controller => "venues", :action => "create" })
+
   get("/venues/:an_id", { :controller => "venues", :action => "show" })
   post("/update_venue/:the_id", { :controller => "application", :action => "show" })
-  get("/delete_venue/:id_to_delete", { :controller => "venue", :action => "destroy" })
+  get("/delete_venue/:venue_id", { :controller => "venues", :action => "destroy" })
   
   get("/insert_comment_record", { :controller => "comments", :action => "create" })
  
